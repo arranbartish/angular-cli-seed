419eb2df3a7f6109611641497cb2e1958a10d1c8
Arran Bartish <abartish@teksystems.com>
2017-02-21 13:28:52 -0500

    resolve query parameter from route and promise

--- src/app/route/search-result/search-result.component.ts
+++ src/app/route/search-result/search-result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-search-result',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.scss']
 })
 export class SearchResultComponent implements OnInit {
+  private term : string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    let param : Observable<string> = this.route
+      .queryParams
+      .map(params => params['q'] || 'None');
+
+    param.subscribe(
+      q => this.term = q,
+      error =>  this.term = <any>error /*should toast*/);
   }
 
 }
