5a3492ac14b09ed564b57eda29d3d36eb844770b
Arran Bartish <abartish@teksystems.com>
2017-02-27 06:36:09 -0500

    test the listing/home component

--- src/app/car/listing/listing.component.spec.ts
+++ src/app/car/listing/listing.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListingComponent } from './listing.component';
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {CarService} from "../service/car.service";
+import {Car} from "../domain/car";
+import {BehaviorSubject, Observable} from "rxjs";
 
 describe('ListingComponent', () => {
+  const carResponse : Car[] = [{
+    brand: 'rolls',
+    model: 'can-ardly',
+    year: '1950',
+    condition: 'almost 70 years old, what do you think?'
+  }];
+
   let component: ListingComponent;
   let fixture: ComponentFixture<ListingComponent>;
 
   beforeEach(async(() => {
+
+    let mockCarsResponse : Observable<Car[]> = new BehaviorSubject(carResponse);
+    let mockedGetCars = jasmine.createSpy("findCars");
+    mockedGetCars.and.returnValue(mockCarsResponse);
+
     TestBed.configureTestingModule({
-      declarations: [ ListingComponent ]
+      declarations: [ ListingComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{
+        provide: CarService,
+        useClass: class {
+          findCars = jasmine.createSpy("findCars");
+          getCars = mockedGetCars;
+        }
+      }]
+
     })
     .compileComponents();
   }));
@@ -30,7 +55,14 @@ describe('ListingComponent', () => {
     });
 
     it('will define options', () => {
-        expect(component.searchOptions).toBeDefined();
+        expect(component.searchOptions).toEqual({
+          name: 'cars',
+          target: './search'
+        });
+    });
+
+    it('will have the car list populated', () => {
+        expect(component.carList).toEqual(carResponse);
     });
   });
 });
