c6618976571a5481e338c420d22743e7077d0198
Florian GOMBERT <fgombert@TEKsystems.com>
2017-03-02 14:32:29 -0500

    Navigation widget #6 (Navigation menu) + Home page + 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {AppComponent} from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {RouterModule, Routes} from '@angular/router';
-import {CarRouteModule} from './car/car.route';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { WidgitModule } from './widgit/widgit.module';
+import { CarRouteModule } from './car/car.route';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './404/pageNotFound.component';
 
 const routes: Routes = [
+  { path: 'car', loadChildren: './car/car.route#CarRouteModule' },
+  { path: 'home', component: HomeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', loadChildren: './car/car.route#CarRouteModule' }
+  { path: '**', component: PageNotFoundComponent }
 ];
-
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -25,6 +30,7 @@ const routes: Routes = [
     HttpModule,
     RouterModule,
     CarRouteModule,
+    WidgitModule,
     NgbModule.forRoot()
   ],
   providers: [],
---
+++ src/app/widgit/navigation/navigation-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { TreeElement } from './valueObject/treeElement';
+import { TreeNode } from './valueObject/treeNode';
+import { TreeLeaf } from './valueObject/treeLeaf';
+
+@Component({
+    selector: 'nav-item',
+    templateUrl: './navigation-item.component.html'
+})
+export class NavItemComponent {
+    @Input('treeElements')
+    public treeElements: TreeElement[];
+
+    constructor(private location: Location) {
+    }
+
+    public isNode(treeElmt: TreeElement): boolean {
+        return typeof (treeElmt as TreeNode).children !== 'undefined';
+    }
+
+    public asNode(treeElmt: TreeElement): TreeNode {
+        return this.isNode(treeElmt) ? (treeElmt as TreeNode) : null;
+    }
+
+    public isActiveNavItem(treeElmt: TreeElement): boolean {
+        return treeElmt.targetUrl === this.location.path();
+    }
+}
