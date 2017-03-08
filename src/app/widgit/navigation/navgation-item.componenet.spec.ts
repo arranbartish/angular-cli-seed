20b537e7ae0826784efb2423ff4e19557ae60072
steptek <monaj7@gmail.com>
2017-03-08 14:59:16 -0500

    adding parametrized test for nav menu.

--- src/app/widgit/navigation/navgation-item.componenet.spec.ts
+++ src/app/widgit/navigation/navgation-item.componenet.spec.ts
@@ -1,27 +1,19 @@
 import { NavigationItemComponent } from './navigation-item.component';
 import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { TestBed, async, ComponentFixture, inject, fakeAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { Location, LocationStrategy } from '@angular/common';
 import * as _ from 'lodash';
-
-
-
-function parameters(dataArray, testCaseFunction) {
-    _.each(dataArray, function (innerArray) {
-        testCaseFunction.apply(this, innerArray);
-    });
-}
+import { TreeNode } from './valueObject/treeNode';
+import { TreeLeaf } from './valueObject/treeLeaf';
+import { TreeElement } from './valueObject/treeElement';
 
 describe('NavigationItemComponent', () => {
     let fixture: ComponentFixture<NavigationItemComponent>;
     let component: NavigationItemComponent;
-    let _aLink: any;
     let cmpLocation: Location;
-
-    const homeLink = { title: 'Home', targetUrl: '/home', imageCssClass: 'glyphicon-home' };
-    const searchLink = { title: 'Search', targetUrl: '/search', imageCssClass: 'glyphicon-search' };
-    const carLink = { title: 'Car', targetUrl: '/car', imageCssClass: 'glyphicon-road' };
+    const inputs = getData();
+    const navMenu = inputs[0];
+    const expected = inputs[1];
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -44,65 +36,79 @@ describe('NavigationItemComponent', () => {
         TestBed.compileComponents().then(() => {
             fixture = TestBed.createComponent(NavigationItemComponent);
             component = fixture.componentInstance;
+            component.treeElements = navMenu;
+            fixture.detectChanges();
         });
         cmpLocation = loc;
     })));
 
 
-     parameters([
-        [homeLink, '/home'],
-        [searchLink, '/search'],
-        [carLink, '/car']
-    ], function (elmt, expected) {
-        it('will generate link for' + expected, async(function () {
-            component.treeElements = [elmt];
-            fixture.detectChanges();
-            fixture.whenStable().then(() => {
-                _aLink = fixture.debugElement.nativeElement.querySelector('a');
-                expect(_aLink.href).toContain(expected);
-            });
-        }));
-    });
+    parameters([
+        [navMenu[0], expected[0]],
+        [navMenu[1], expected[1]],
+        [navMenu[2], expected[2]]],
+        (elmt: TreeElement, result) => {
+
+            it('will generate link for ' + elmt.targetUrl, async(() => {
+                fixture.whenStable().then(() => {
+                    const links = _.filter(fixture.debugElement.children, (del: DebugElement) => {
+                        return del.children[0].nativeElement.attributes.id.value === elmt.title;
+                    });
+                    const _aLink = links[0].nativeElement.querySelector('a');
+                    expect(_aLink.href).toContain(elmt.targetUrl);
+                });
+            }));
+
+            it('will verify if  ' + elmt.title + '  is a node', async(() => {
+                fixture.whenStable().then(() => {
+                    expect(component.isNode(elmt)).toBe(result.isNode);
+                });
+            }));
 
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+            it('will verify if  ' + elmt.title + '  is as node', async(() => {
+                fixture.whenStable().then(() => {
+                    expect(component.asNode(elmt)).toBe(result.asNode);
+                });
+            }));
 
 
-    it('will call isNode when asNode is called with same element', () => {
-        const isNode = spyOn(component, 'isNode').and.callThrough();
-        const asNode = component.asNode(homeLink);
-        expect(isNode).toHaveBeenCalled();
-        expect(asNode).toBe(null);
-    });
-
-
-    it('will return false when no children element', () => {
-        expect(component.isNode(homeLink)).toBe(false);
-    });
-
-
-    it('will return true when element has children', () => {
-        const homeChildren: any = homeLink;
-        homeChildren.children = [{}];
-        expect(component.isNode(homeChildren)).toBe(true);
-    });
-
-
-    it('will not activate the element if it\'s not clicked', () => {
-        expect(component.isActiveNavItem(homeLink)).toBe(false);
-    });
-
-
-    it('will show search as active link', () => {
-        const spyLocation: any = cmpLocation.path;
-
-        const expected = searchLink.targetUrl;
-        spyLocation.and.returnValue('/search');
-        expect(component.isActiveNavItem(searchLink)).toBe(true);
-    });
+            it('will verify if  ' + elmt.title + '  is active', async(() => {
+                fixture.whenStable().then(() => {
+                    const spyLocation: jasmine.Spy = <jasmine.Spy>cmpLocation.path;
+                    const isActive: boolean = result.isActive;
+                    if (isActive) {
+                        spyLocation.and.returnValue(elmt.targetUrl);
+                    }
+                    expect(component.isActiveNavItem(elmt)).toBe(result.isActive);
+                });
+            }));
+        });
 
 });
+
+// Helper functions.
+function parameters(inputData, execFunction) {
+    _.each(inputData, (record) => {
+        execFunction.call(this, record[0], record[1]);
+    });
+}
+
+
+function getData(): Array<any> {
+    const homeNode: TreeElement = { title: 'Home', targetUrl: '/home', imageCssClass: 'glyphicon-home' };
+    const searchNode: TreeElement = { title: 'Search', targetUrl: '/search', imageCssClass: 'glyphicon-search' };
+    const brande: TreeLeaf = { title: 'Brand', targetUrl: '/car/brand', imageCssClass: 'glyphicon-road' };
+    const type: TreeLeaf = { title: 'Type', targetUrl: '/car/type', imageCssClass: 'glyphicon-road' };
+    const carNode: TreeNode = { title: 'Car', targetUrl: '/car', children: [brande, type], imageCssClass: 'glyphicon-road' };
+    const menu: Array<TreeElement> = [homeNode, searchNode, carNode];
+
+    const expectedTable: Array<any> = [
+        { isNode: false, isActive: false, asNode: null },
+        { isNode: false, isActive: false, asNode: null },
+        { isNode: true, isActive: true, asNode: carNode },
+    ];
+    return [menu, expectedTable];
+}
 
 
