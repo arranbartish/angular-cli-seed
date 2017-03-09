7cc8ffffd87f4751e1deeeb0b6e8b2789051c1f0
Moksit <nmoksit@teksystems.com>
2017-03-09 16:21:14 -0500

    adding unit test for ngrx-effects

--- src/app/car/effects/cars..ts
+++ src/app/car/effects/cars..ts
@@ -1,11 +1,11 @@
 
-import {Injectable} from '@angular/core';
-import {Effect, Actions, toPayload} from '@ngrx/effects';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Effect, Actions, toPayload } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import {Action} from '@ngrx/store';
-import {CarService} from '../service/car.service';
-import {CarAction, ActionFactory} from '../actions/cars';
+import { Action } from '@ngrx/store';
+import { CarService } from '../service/car.service';
+import { CarAction, ActionFactory } from '../actions/cars';
 import { empty } from 'rxjs/observable/empty';
 
 @Injectable()
@@ -18,7 +18,7 @@ export class CarEffects {
     .map(toPayload)
     .switchMap(searchTerm => {
       if (searchTerm === '') {
-        return empty();
+        return of(ActionFactory.clearCars()); //empty();
       }
 
       const nextSearch$ = this.actions$.ofType(CarAction.SEARCH).skip(1);
@@ -26,8 +26,10 @@ export class CarEffects {
       return this.carService.findCars(searchTerm)
         .takeUntil(nextSearch$)
         .map(result => ActionFactory.searchComplete(result))
-        .catch(of(ActionFactory.clearCars()));
-    });
+        .catch(error => {
+          return of(ActionFactory.clearCars());  /*of(ActionFactory.clearCars())*/
+        });
+  });
 
   constructor(private actions$: Actions, private carService: CarService) { }
 }
