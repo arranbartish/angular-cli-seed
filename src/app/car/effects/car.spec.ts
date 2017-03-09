cac629042ce294d387754b5918c77da037d61b91
Moksit <nmoksit@teksystems.com>
2017-03-09 16:21:14 -0500

    adding unit test for ngrx-effects

--- src/app/car/effects/car.spec.ts
+++ src/app/car/effects/car.spec.ts
@@ -1,21 +1,22 @@
-import {inject, TestBed} from '@angular/core/testing';
-import {Car, CarState} from '../domain/car';
-import {CarModule} from '../car.module';
-import {CarService} from '../service/car.service';
-import {StoreModule, Store} from '@ngrx/store';
-import {cars} from '../reducers/car.reducer';
-import {ActionFactory} from '../actions/cars';
-import {Observable, BehaviorSubject} from 'rxjs';
-import {CarEffects} from './cars.';
-import {EffectsTestingModule} from '@ngrx/effects/testing';
+import { empty } from 'rxjs/observable/empty';
+import { inject, TestBed, async, fakeAsync, tick, discardPeriodicTasks } from '@angular/core/testing';
+import { Car, CarState } from '../domain/car';
+import { CarModule } from '../car.module';
+import { CarService } from '../service/car.service';
+import { StoreModule, Store } from '@ngrx/store';
+import { cars } from '../reducers/car.reducer';
+import { ActionFactory, CarAction, ListCarsAction } from '../actions/cars';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { CarEffects } from './cars.';
+import { EffectsTestingModule, EffectsRunner } from '@ngrx/effects/testing';
 
-
-describe('CarEffects', () => {
+fdescribe('CarEffects', () => {
 
   let mockCarService: CarService;
   let effect: CarEffects;
   let store: Store<CarState>;
   let subscribedCars: Car[];
+  let executor: EffectsRunner;
 
   const mockResponse = [{
     brand: 'Toyota',
@@ -27,25 +28,27 @@ describe('CarEffects', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CarModule, EffectsTestingModule, StoreModule.provideStore({cars})],
+      imports: [CarModule, EffectsTestingModule, StoreModule.provideStore({ cars })],
       providers: [
         CarEffects,
         {
           provide: CarService,
           useClass: class {
-            findCars(term: string): Observable<Car[]> { return new BehaviorSubject([]) }
-            getCars(): Observable<Car[]> { return new BehaviorSubject([]) }
+            findCars(term: string): Observable<Car[]> { return new BehaviorSubject([]) };
+            getCars(): Observable<Car[]> { return new BehaviorSubject([]) };
           }
         }
       ]
     });
   });
 
-  beforeEach(inject([CarService, CarEffects, Store], (carService: CarService, carEffects: CarEffects, _store: Store<CarState>) => {
+  beforeEach(fakeAsync(inject([CarService, CarEffects, Store, EffectsRunner], 
+            (carService: CarService, carEffects: CarEffects, _store: Store<CarState>, _runner: EffectsRunner) => {
     mockCarService = carService;
     effect = carEffects;
     store = _store;
-  }));
+    executor = _runner;
+  })));
 
   beforeEach(() => {
     store.select(state => state.cars).subscribe(carsList => subscribedCars = carsList);
@@ -58,15 +61,55 @@ describe('CarEffects', () => {
 
 
   it('will be injected with the mock car service', () => {
-      expect(mockCarService).toBeDefined();
+    expect(mockCarService).toBeDefined();
   });
 
   it('will be able to get the effect to test', () => {
-      expect(effect).toBeDefined();
+    expect(effect).toBeDefined();
   });
 
   it('will be injected with the store', () => {
-      expect(store).toBeDefined();
+    expect(store).toBeDefined();
+  });
+
+  it('will be injected with the effect executor', () => {
+    expect(executor).toBeDefined();
+  });
+
+  it('will return same search action', (done) => {
+    executor.queue({ type: CarAction.SEARCH });
+    effect.search$.subscribe(result => {
+      expect(result.type).toBe('Car - list cars');
+      done();
+    });
+  });
+
+  it('will return an empty search result by calling car service', (done) => {
+    executor.queue({ type: CarAction.SEARCH, payload: 'Ford' });
+    effect.search$.subscribe(result => {
+      expect(result.payload.length).toBe(0);
+      done();
+    });
+  });
+
+
+  it('will return a filled result', (done) => {
+    spyOn(mockCarService, 'findCars').and.returnValue(new BehaviorSubject(mockResponse));
+    executor.queue({ type: CarAction.SEARCH, payload: 'Toyota' });
+    effect.search$.subscribe(result => {
+      expect(result.payload).toBe(mockResponse);
+      done();
+    });
+  });
+
+
+  it('will return empty result when no search term provided', (done) => {
+    executor.queue({ type: CarAction.SEARCH, payload: '' });
+    effect.search$.subscribe((result) => {
+      expect(result.payload.length).toBe(0);
+      done();
+    });
+
   });
 
 });
