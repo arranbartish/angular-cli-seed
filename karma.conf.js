4c4049471d1d2ae1a7ce7c771edef194c05978dd
Arran Bartish <arranbartish@teksystems.com>
2017-03-14 15:54:21 -0400

    actually exclude test.ts

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,6 @@ module.exports = function (config) {
     istanbulThresholdReporter: {
       src: 'coverage/coverage-final.json',
       reporters: ['text'],
-      excludes: ['src/test.ts'],
       thresholds: {
         global: {
           statements: 98,
