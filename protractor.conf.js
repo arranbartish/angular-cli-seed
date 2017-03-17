b22615a34c5ecae5ee7caec9868eb2c5fe7f95c9
Angular CLI <angular-cli@angular.io>
2017-03-17 09:55:15 -0400

    chore: upgrade commit from @angular/cli

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,32 +1,23 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-//const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  baseUrl: 'http://localhost:4200/',
-  //chromeOnly: true,
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
+  baseUrl: 'http://localhost:4200/',
   framework: 'mocha',
-  // jasmineNodeOpts: {
-  //   showColors: true,
-  //   defaultTimeoutInterval: 30000,
-  //   print: function() {}
-  // },
   mochaOpts: {
     reporter: "spec",
     slow: 3000,
     ui: 'bdd',
     timeout: 30000
-    //bail: true 5143805700
   },
   beforeLaunch: function() {
     require('ts-node').register({
