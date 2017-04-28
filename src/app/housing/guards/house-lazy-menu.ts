b89b193e50f85e921dfbf6099d14638e054c7013
Florian GOMBERT <fgombert@TEKsystems.com>
2017-04-28 14:21:54 -0400

    fixing the linting issues + making the lazy menu loading ( oh yeah \o/ ) + will fail the build ( as "arranbartish-angular-cli-widgets v4.0.4" still doesn't exist ) -- yiiiissssss

--- src/app/housing/guards/house-lazy-menu.ts
+++ src/app/housing/guards/house-lazy-menu.ts
@@ -7,47 +7,48 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { House, HousesState } from '../domain/housing';
-import { HouseService } from '../service/house.service';
 
 @Injectable()
 export class HousesLazyMenuGuard implements CanActivate {
 
   constructor(private menuStore: Store<MenuState>,
-    private houseService: HouseService) {
+              private housesStore: Store<HousesState>) {
   }
 
   initializeMenu(): Observable<boolean> {
-    var housingItems: House[] = [];
-    //this.housesStore.select(state => state.houses).subscribe((value: House[]) => housingItems = value);
-    this.houseService.getHouses().map((value: House[]) => housingItems = value);
+    this.housesStore.select(state => state.houses).subscribe((housingItems: House[]) => {
 
-    const housingMenuTitle = 'Housing';
-    var housingMenu = {
-      title: housingMenuTitle,
-      targetUrl: '/housing',
-      imageCssClass: 'glyphicon-home',
-      children: [
-        { title: 'Search houses', targetUrl: '/housing/search', imageCssClass: 'glyphicon glyphicon-search' },
-        ...(housingItems.map(house => <TreeElement>{ title: house.country, targetUrl: '/housing/search?q=' + house.country, imageCssClass: 'glyphicon glyphicon-list' }))
-      ]
-    };
+      const housingMenuTitle = 'Housing';
+      const housingMenu = {
+        title: housingMenuTitle,
+        targetUrl: '/housing',
+        imageCssClass: 'glyphicon-home',
+        children: [
+          { title: 'Search houses', targetUrl: '/housing/search', imageCssClass: 'glyphicon glyphicon-search' },
+          ...(
+            housingItems.map(house => <TreeElement>
+              { title: house.country, targetUrl: '/housing/search?q=' + house.country, imageCssClass: 'glyphicon glyphicon-list' })
+              .sort((treeElmA, treeElmB) => treeElmA.title > treeElmB.title ? 1 : 0)
+          )
+        ]
+      };
 
-    var menuItems: TreeElement[] = [];
-    this.menuStore.select(state => state.treeElements).subscribe((value: TreeElement[]) => menuItems = value);
+      let menuItems: TreeElement[] = [];
+      this.menuStore.select(state => state.treeElements).subscribe((value: TreeElement[]) => menuItems = value);
 
-    var theHousingItemIndex = -1;
-    menuItems.filter((value: TreeElement, index: number) => {
-      if (value.title === housingMenuTitle) theHousingItemIndex = index;
-      return value.title === housingMenuTitle;
+      let theHousingItemIndex = -1;
+      menuItems.filter((value: TreeElement, index: number) => {
+        theHousingItemIndex = value.title === housingMenuTitle ? index : theHousingItemIndex;
+        return value.title === housingMenuTitle;
+      });
+
+      if (theHousingItemIndex === -1) {
+        this.menuStore.dispatch(MenuActionFactory.addMenuItems([housingMenu]));
+      } else {
+        menuItems[theHousingItemIndex] = housingMenu;
+        this.menuStore.dispatch(MenuActionFactory.setMenuItems(menuItems));
+      }
     });
-
-    if (theHousingItemIndex == -1) {
-      this.menuStore.dispatch(MenuActionFactory.addMenuItems([housingMenu]));
-    }
-    else {
-      menuItems[theHousingItemIndex] = housingMenu;
-      this.menuStore.dispatch(MenuActionFactory.setMenuItems(menuItems));
-    }
 
     return of(true);
   }
